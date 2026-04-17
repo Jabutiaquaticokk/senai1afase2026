cdfd089d2a571cc126d0f685c4ca38d41fe95ab9
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-04-16 22:07:52 -0300

    Java script :D

---
+++ 01-JS/script.js
@@ -0,0 +1,14 @@
+// alert("Hello World!")
+// alert("Soy Japones, de Minas Gerais")
+let sabonete
+let idade = 17
+
+// sabonete = prompt("Digite a marca do sabonete que você usa:")
+// alert("Você usa " + sabonete)
+
+
+let nota1
+nota1 = prompt("Nota 1")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
