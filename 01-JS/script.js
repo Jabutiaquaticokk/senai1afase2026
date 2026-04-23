6be0cfdbb00503435d44081a284f02b05db25c57
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-04-23 20:29:11 -0300

    Atividade

--- 01-JS/script.js
+++ 01-JS/script.js
@@ -1,14 +1,30 @@
-// alert("Hello World!")
-// alert("Soy Japones, de Minas Gerais")
-let sabonete
-let idade = 17
+// // alert("Hello World!")
+// // alert("Soy Japones, de Minas Gerais")
+// let sabonete
+// let idade = 17
 
-// sabonete = prompt("Digite a marca do sabonete que você usa:")
-// alert("Você usa " + sabonete)
+// // sabonete = prompt("Digite a marca do sabonete que você usa:")
+// // alert("Você usa " + sabonete)
 
 
-let nota1
-nota1 = prompt("Nota 1")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+// let nota1
+// nota1 = prompt("Nota 1")
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// alert(nota1)
+
+//==========
+
+// Info
+let wins, draw
+let points
+// Read
+wins = prompt("Número de vitorias: ")
+wins = Number(wins)
+draw = prompt("Empates: ")
+draw = Number(draw)
+// process
+points = wins * 3 + draw
+// saidas
+alert("Total Pontos: " + points)
+//==========
