b68a621b89a35d75c44cb883a5ed097a834cc76b
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-04-27 20:18:57 -0300

    Segunda fera

--- 01-JS/monika.js
+++ 01-JS/monika.js
@@ -4,5 +4,5 @@ let chance
 
 n=Number(prompt("Quantas Vezes Usou o celular?"))
 chance = (0.1/(1+500* n))*100
-alert("Sua chance é de " + chance.toFixed(1) + "%")
+alert("Sua chance é de " + chance.toFixed(5) + "%")
 console.log(n)
