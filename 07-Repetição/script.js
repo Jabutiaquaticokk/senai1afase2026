bcd914bf5a50e53c0288bcbc3b119344cdccd601
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-06-15 22:15:26 -0300

    retecipoes

--- 07-Repetição/script.js
+++ 07-Repetição/script.js
@@ -1,6 +1,19 @@
-let contador=0
+// let contador=0 //inicialização
+// //(cond repetição)
+// while(contador<10){
+//    alert('Contador: '+contador)
+//    contador++//contagem
+// }//volta e testa novamente a condição
 
-while(contador<10){
-   alert('Contador: '+contador)
-   contador++
-}//volta e testa novamente a condição
+let peso
+
+
+let saco=0
+while(saco<10){
+    peso=Number(prompt('Digite o Peso'))
+
+if(peso>1.1||peso<0.9){
+    alert('fora da tolerância do peso')
+}
+    saco++
+}
