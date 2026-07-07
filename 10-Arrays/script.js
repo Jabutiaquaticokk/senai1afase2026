7d7a1753308bbc953c9a983d6707ad8ab04197cb
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-07-06 21:05:41 -0300

    bem dificil

--- 10-Arrays/script.js
+++ 10-Arrays/script.js
@@ -200,36 +200,4 @@ let city=['Jaraguá', 'Joinville', 'Blumenau']
 function santacatarina(){
     document.getElementById('resultados2').innerHTML='Talvez esteja em '+city[0]+', '+city[1]+' ou '+city[2]+'. Lá por Santa Catarina[...]'
 }
-        const personagens = [
-            "Rivaldo Jesus",
-            "Kowalski",
-            "Genezio",
-            "Waltdisney"
-        ];
-
-        const lista = document.getElementById("lista");
-        const seletor = documetn.getElementById("seletor");
-
-        personagens.forEach((nome, i) => {
-            const celula = document.createElement("div");
-            celula.className = "celula";
-            celula.id = "item" + i;
-            celula.innerHTML = `<span class="valor">${nome}</span><class="idx">[${i}]</span>`
-            lista.appendChild(celula);
-
-            const opt = document.createElement("option");
-            opt.value = nome;
-            opt.textContent = nome;
-            seletor.appendChild(opt);
-        });
-
-        function buscarIndice() {
-            document.querySelectorAll(".celula").foreach(c.classLista.remove("destaque"));
-            const nomeEscolhido = selector.value;
-            const indice = personagens.indexOf(nomeEscolhido);
-            document.getElementById("resultado").textContent =
-
-                `"${nomeEscolhido}" está no indice ${indice}`;
-            document.getElementById("item-" + indice).classList.add("destaque");
-
-        }
+   
