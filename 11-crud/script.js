55500ee0b9373b4c525998808ca840ae1e26c4c5
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-07-14 20:14:56 -0300

    inputs

---
+++ 11-crud/script.js
@@ -0,0 +1,34 @@
+// C >> CREATE 
+// R >> READ 
+// U >> UPDATE 
+// D >> DELETE
+
+// let dino={
+//     nome: "T-Rex",
+//     altura: 3.5,
+//     cor: "Verde",
+//     valor: 1000000
+    
+
+// }
+
+// console.log(dino);
+
+// const nome=[]
+// const altura=[]
+// const cor=[]
+// const valor=[]
+
+const dinos=[]
+
+function teste(){
+    let dino={
+    nome: "T-Rex",
+    altura: 3.5,
+    cor: "Verde",
+    valor: 1000000
+    }
+
+    dinos.push(dino)
+}
+
