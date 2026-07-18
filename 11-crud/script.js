d77895160a6f7b7727dac841c7f8e2c61d50ea4d
Jabutiaquaticokk <joseesdrasandradearaujo@gmail.com>
2026-07-17 21:42:04 -0300

    eu sou muito foda

--- 11-crud/script.js
+++ 11-crud/script.js
@@ -92,7 +92,55 @@ function mostrarDados(){
     }
 }
 
+function limparForm(){
+ document.getElementById('input-nome').value=''
+  document.getElementById('input-altura').value=''
+  document.getElementById('input-cor').value=''
+  document.getElementById('input-valor').value=''
+  document.getElementById('input-id').value=''
+  
+}
+
+function pesquisar(){
+  document.getElementById('p-dados').innerHTML=''
+  let nomeProcurado=document.getElementById('input-nome').value
+  for(let i=0;i<nomes.length;i++){
+      if(nomeProcurado==nomes[i].nome){
+      document.getElementById('input-nome').value=nomes[i].nome
+      document.getElementById('input-altura').value=nomes[i].altura
+      document.getElementById('input-cor').value=nomes[i].cor
+      document.getElementById('input-valor').value=nomes[i].valor
+      document.getElementById('input-id').value=nomes[i].id
+    }
+  }
+}
+
+function salvar(){
+  let id=Number(document.getElementById('input-id').value)
+  for(let i=0;i<nomes.length;i++){
+    if(id==nomes[i].id){
+      nomes[i].nome=document.getElementById('input-nome').value
+      nomes[i].altura=document.getElementById('input-altura').value
+      nomes[i].cor=document.getElementById('input-cor').value
+      nomes[i].valor=document.getElementById('input-valor').value
+      nomes[i].id=document.getElementById('input-id').value
+    }
+  }
+  mostrarDados()
+  limparForm()
+}
+
 function removerDino(){
- 
+  let id=Number(document.getElementById('input-id').value)
+  for(let i=0;i<nomes.length;i++){
+    if(id==nomes[i].id){
+      nomes.splice(i,1)
+    }
+    
+    
+    
+  }
+  mostrarDados()
+  limparForm()
   
 }
